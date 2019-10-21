1394c21970eaf2c39e2c1a62e080dedfaadd5dc4
Jonathan Owah <owahjonathan@gmail.com>
2019-10-21 22:07:25 +0100

    Revert "Deploy"

--- README.md
+++
@@ -1,3 +0,0 @@
-<h1># aad-team-36-animation-challenge</h1>
-A Quiz-based android application applying various animations. 
-<p>Built by learners in the Andela Learning Community 4.0 <strong> Team AAD 36</strong>.</p>
--- app/src/main/java/io/github/jokurio/aqa/AndroidQuiz.java
+++ app/src/main/java/io/github/jokurio/aqa/AndroidQuiz.java
@@ -3,21 +3,15 @@ package io.github.jokurio.aqa;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.animation.Animator;
-import android.animation.AnimatorInflater;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.RadioButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class AndroidQuiz extends AppCompatActivity {
-    public static int finalScore;
     RadioButton linux;
     RadioButton windows;
     RadioButton bluetooth;
@@ -36,93 +30,51 @@ public class AndroidQuiz extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.android_quiz);
     }
-    /**The following 6 methods(kernel, drivers, intent,ANR, APK and ALC)  animates
-     * the images besides each question as well as the image at the top of the screen
-     */
-    public void kernel(View view){
-        ImageView imag = findViewById(R.id.imageView);
-        ImageView image = findViewById(R.id.img1);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
-                R.anim.move);
-        image.startAnimation(animation);
-        imag.startAnimation(animation);
-
-        zoomSelection(view);
-    }
-    public void drivers(View view){
-        ImageView imag = findViewById(R.id.imageView);
-        ImageView image = findViewById(R.id.img2);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
-                R.anim.slide);
-        image.startAnimation(animation);
-        imag.startAnimation(animation);
-
-        zoomSelection(view);
-    }
-    public void intent(View view){
-        ImageView imag = findViewById(R.id.imageView);
-        ImageView image = findViewById(R.id.img3);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
-                R.anim.blink);
-        image.startAnimation(animation);
-        imag.startAnimation(animation);
-
-        zoomSelection(view);
-    }
-    public void ANR(View view){
-        ImageView imag = findViewById(R.id.imageView);
-        ImageView image = findViewById(R.id.img4);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
-                R.anim.fade);
-        image.startAnimation(animation);
-        imag.startAnimation(animation);
-
-        zoomSelection(view);
-    }
-    public void APK(View view){
-        ImageView imag = findViewById(R.id.imageView);
-        ImageView image = findViewById(R.id.img5);
+    public void clockwise(View view){
+        ImageView image = findViewById(R.id.imageView);
         Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
                 R.anim.animation);
         image.startAnimation(animation);
-        imag.startAnimation(animation);
-
-        zoomSelection(view);
-    }
-    public void ALC(View view){
-        ImageView imag = findViewById(R.id.imageView);
-        ImageView image = findViewById(R.id.img6);
-        ImageView alc = findViewById(R.id.alcImage);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
-                R.anim.blink);
-        image.startAnimation(animation);
-        imag.startAnimation(animation);
-        alc.startAnimation(animation);
-
-        zoomSelection(view);
     }
 
-    //animates the 'bluetooth' option, so it's animation is different from that of the wifi
-    public void wifi(View view){
-        ImageView imag = findViewById(R.id.imageView);
-        ImageView image = findViewById(R.id.img2);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
-                R.anim.animation);
-        image.startAnimation(animation);
-        imag.startAnimation(animation);
-
-        zoomSelection(view);
+    public void zoom(View view){
+        ImageView image = findViewById(R.id.imageView);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.clockwise);
+        image.startAnimation(animation1);
     }
-    public void submitQuiz(View view) {
-        //Animates the image at the top
+
+    public void fade(View view){
+        ImageView image = findViewById(R.id.imageView);
+        Animation animation1 =
+                AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.fade);
+        image.startAnimation(animation1);
+    }
+
+    public void blink(View view){
         ImageView image = findViewById(R.id.imageView);
         Animation animation1 =
                 AnimationUtils.loadAnimation(getApplicationContext(),
                         R.anim.blink);
         image.startAnimation(animation1);
     }
-    public void submitQuiz(View view) {
 
+    public void move(View view){
+        ImageView image = findViewById(R.id.imageView);
+        Animation animation1 =
+                AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+        image.startAnimation(animation1);
+    }
+
+    public void slide(View view){
+        ImageView image = findViewById(R.id.imageView);
+        Animation animation1 =
+                AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide);
+        image.startAnimation(animation1);
+    }
+    public void submitQuiz(View view) {
+        int finalScore;
         int quizOne = 0;
         int quizTwo = 0;
         int quizThree = 0;
@@ -130,68 +82,93 @@ public class AndroidQuiz extends AppCompatActivity {
         int quizFive = 0;
         int quizSix = 0;
         Boolean Linux;
+        Boolean Windows;
         linux = findViewById(R.id.linux);
+        windows = findViewById(R.id.windows);
         Linux = linux.isChecked();
+        Windows = windows.isChecked();
         if (Linux) {
             quizOne = 5;
+        } else if (Windows) {
+            quizOne = 0;
         }
-
+        Boolean Bluetooth;
         Boolean Wifi;
+        bluetooth = findViewById(R.id.bluetooth);
         wifi = findViewById(R.id.wifi);
+        Bluetooth = bluetooth.isChecked();
         Wifi = wifi.isChecked();
-        if (Wifi) {
+        if (Bluetooth) {
+            quizTwo = 0;
+        } else if (Wifi) {
             quizTwo = 5;
         }
-
         Boolean Implicit;
+        Boolean Explicit;
         implicit = findViewById(R.id.implicit);
+        explicit = findViewById(R.id.explicit);
         Implicit = implicit.isChecked();
+        Explicit = explicit.isChecked();
         if (Implicit) {
             quizThree = 5;
+        } else if (Explicit) {
+            quizThree = 0;
         }
-
+        Boolean ANR_ONE;
         Boolean ANR_TWO;
+        ANR1 = findViewById(R.id.anr1);
         ANR2 = findViewById(R.id.anr2);
+        ANR_ONE = ANR1.isChecked();
         ANR_TWO = ANR2.isChecked();
-        if (ANR_TWO) {
+        if (ANR_ONE) {
+            quizFour = 0;
+        } else if (ANR_TWO) {
             quizFour = 5;
         }
-
+        Boolean Processing;
         Boolean Packaging;
+        APK1 = findViewById(R.id.apk1);
         APK2 = findViewById(R.id.apk2);
+        Processing = APK1.isChecked();
         Packaging = APK2.isChecked();
-        if (Packaging) {
+        if (Processing) {
+            quizFive = 0;
+        }else if (Packaging) {
             quizFive = 5;
         }
-
         Boolean Andela;
+        Boolean Adult;
         ALC1 = findViewById(R.id.alc1);
+        ALC2 = findViewById(R.id.alc2);
         Andela = ALC1.isChecked();
+        Adult = ALC2.isChecked();
         if (Andela) {
             quizSix = 5;
         }
+        if (Adult) {
+            quizSix = 0;
+        }
+
         finalScore = (quizOne + quizTwo + quizThree + quizFour + quizFive + quizSix);
 
-        Intent intent = new Intent(AndroidQuiz.this, ScoreActivity.class);
-        startActivity(intent);
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-
+        if (finalScore == 30) {
+            Toast.makeText(this, "Perfect!You got all 6 questions right, " + finalScore + " points for you.", Toast.LENGTH_LONG).show();
+        } else if (finalScore == 25) {
+            Toast.makeText(this, "Very good!You answered 5 questions correctly, " + finalScore + " points for you.", Toast.LENGTH_LONG).show();
+        } else if (finalScore == 20) {
+            Toast.makeText(this, "Good!You got 4 questions right, " + finalScore + " points for you.", Toast.LENGTH_LONG).show();
+        } else if (finalScore == 15) {
+            Toast.makeText(this, "Okay, you got 3 questions right, " + finalScore + " points for you", Toast.LENGTH_LONG).show();
+        } else if (finalScore == 10) {
+            Toast.makeText(this, "Just " + finalScore + " points out of 30. You can do better.", Toast.LENGTH_LONG).show();
+        } else if (finalScore == 5) {
+            Toast.makeText(this, +finalScore + " points out of 30. Not too good.", Toast.LENGTH_LONG).show();
+        } else if (finalScore == 0) {
+            Toast.makeText(this, +finalScore + " points out of 30. Bad day, or geography is just not your thing?", Toast.LENGTH_LONG).show();
+        }
     }
-    //This method restarts the quiz by resetting all the values
-    public void resetQuiz(View v) {
-        linux = findViewById(R.id.linux);
-        windows = findViewById(R.id.windows);
-        bluetooth = findViewById(R.id.bluetooth);
-        wifi = findViewById(R.id.wifi);
-        implicit = findViewById(R.id.implicit);
-        explicit = findViewById(R.id.explicit);
-        ANR1 = findViewById(R.id.anr1);
-        ANR2 = findViewById(R.id.anr2);
-        APK1 = findViewById(R.id.apk1);
-        APK2 = findViewById(R.id.apk2);
-        ALC1 = findViewById(R.id.alc1);
-        ALC2 = findViewById(R.id.alc2);
 
+    public void resetQuiz(View v) {
         linux.setChecked(false);
         windows.setChecked(false);
         bluetooth.setChecked(false);
@@ -204,19 +181,6 @@ public class AndroidQuiz extends AppCompatActivity {
         APK1.setChecked(false);
         ANR2.setChecked(false);
         ANR1.setChecked(false);
-
-        //This rotates the image while the quiz is being restarted
-        ImageView image = findViewById(R.id.imageView);
-        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(),
-                R.anim.clockwise);
-        image.startAnimation(animation1);
     }
 
-    // helper function to zoom selected radiobuttons
-    private void zoomSelection(View view){
-        Animator animator = AnimatorInflater.loadAnimator(this, R.animator.zoom);
-        animator.setTarget(view);
-        animator.start();
-    }
 }
-
